d329c770eb850477255cf459ea433e8c331266e6
Thorbjörn Albrecht <thorbjoern.albrecht@tuhh.de>
2025-08-04 16:48:15 +0200

    added comments to conceptualize gui

---
+++ satellite_tracker/gui.py
@@ -0,0 +1,10 @@
+# display orbit
+# create space background
+# add buttons to click
+# button to load an example iss tle
+# add line to upload tle file
+# round earth
+# small egg/satellite
+# light and dark more?
+# see pi status and ip
+# 
