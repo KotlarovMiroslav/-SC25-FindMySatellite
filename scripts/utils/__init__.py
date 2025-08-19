fd608b992c389d597a05adf2e642602ae0ea5d23
KotlarovMiroslav <kotlarovmiroslav@gmail.com>
2025-08-19 11:08:47 +0300

    Finally a working demo - congrats, wild milestone

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -2,4 +2,4 @@ from .lidarUtils import data_formatter
 from .servoUtils import set_angle
 from .classes import State
 from .classes import Operator
-from .lidar_thread import lidar_reader
+from .lidar_reader import lidar_reader
